7e353b159636e8c06acfe82ab8c29effc310f137
bayupeyy <mochamadbayuajie@gmail.com>
2024-05-30 15:10:45 +0700

    kilobite

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"kilobite/user"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "root:@tcp(127.0.0.1:3306)/kilobite?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Koneksi Berhasil")
+
+	var users []user.User
+
+	db.Find(&users)
+}
