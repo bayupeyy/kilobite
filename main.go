45349775372cc9f3e5d5ff53e48ecd92dfef8565
bayupeyy <mochamadbayuajie@gmail.com>
2024-05-30 21:35:56 +0700

    gin

--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"kilobite/user"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	// 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	// 	dsn := "root:@tcp(127.0.0.1:3306)/kilobite?charset=utf8mb4&parseTime=True&loc=Local"
+	// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// 	if err != nil {
+	// 		log.Fatal(err.Error())
+	// 	}
+
+	// 	fmt.Println("Koneksi Berhasil")
+
+	// 	var users []user.User
+	// 	db.Find(&users)
+
+	// 	for _, users := range users {
+	// 		fmt.Println(users.Name)
+	// 		fmt.Println(users.Email)
+	// 		fmt.Println("===========")
+	// 	}
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/kilobite?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -18,9 +43,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Koneksi Berhasil")
-
 	var users []user.User
-
 	db.Find(&users)
+
+	c.JSON(http.StatusOK, users)
 }
