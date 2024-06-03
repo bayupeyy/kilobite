1d066548dbde3125f0536491a707ebb527d2df28
bayupeyy <mochamadbayuajie@gmail.com>
2024-06-03 20:12:58 +0700

    handler

--- handler/user.go
+++ handler/user.go
@@ -1,1 +1,21 @@
 package handler
+
+import (
+	"kilobite/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	//Tangkap input dari user
+	//Map input dari user ke struct RegisterUserInput
+	//struct di atas kita parsing sebagai parameter service
+}
