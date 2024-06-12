0b48a7903cbfa071dd494917120f7ddf0ff16bb1
unknown <mochamadbayuajie@gmail.com>
2024-06-12 22:23:47 +0700

    validasi token

--- auth/service.go
+++ auth/service.go
@@ -1,15 +1,20 @@
 package auth
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"errors"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
 
 type jwtService struct {
 }
 
-//Membuat Secret Key
+// Membuat Secret Key
 var secretKey = []byte("koclok_clok")
 
 func NewService() *jwtService {
@@ -32,4 +37,23 @@ func (s *jwtService) GenerateToken(userID int) (string, error) {
 	}
 
 	return signedToken, nil
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	//Parse
+	token, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Invalid token")
+		}
+
+		return []byte(secretKey), nil
+	})
+
+	//Pengecekan error
+	if err != nil {
+		return token, err
+	}
+	return token, nil
 }
