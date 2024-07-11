07c0a2d4eda53e25661285917c34d85ca9067529
unknown <mochamadbayuajie@gmail.com>
2024-07-12 05:55:34 +0700

    chore: update repo

--- campaign/repository.go
+++ campaign/repository.go
@@ -10,6 +10,7 @@ type Repository interface {
 	FindByID(ID int) (Campaign, error)
 	Save(campaign Campaign) (Campaign, error)
 	Update(campaign Campaign) (Campaign, error)
+	CreateImage(campaignImage CampaignImage) (Campaign, error)
 }
 
 type repository struct {
@@ -72,4 +73,14 @@ func (r *repository) Update(campaign Campaign) (Campaign, error) {
 	}
 
 	return campaign, nil
+}
+
+// Implementasi untuk CreateImage
+func (r *repository) CreateImage(campaignImage CampaignImage) (CampaignImage, error) {
+	err := r.db.Create(&campaignImage).Error
+	if err != nil {
+		return campaignImage, err
+	}
+
+	return campaignImage, nil
 }
