a265ef38799b745ddf3b8b86767ad7f931883acb
bayupeyy <mochamadbayuajie@gmail.com>
2024-07-11 14:05:46 +0700

    update campaign endpoint

--- campaign/service.go
+++ campaign/service.go
@@ -1,6 +1,7 @@
 package campaign
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/gosimple/slug"
@@ -85,6 +86,10 @@ func (s *service) UpdateCampaign(inputID GetCampaignDetailInput, inputData Creat
 	campaign, err := s.repository.FindByID(inputID.ID)
 	if err != nil {
 		return campaign, err
+	}
+
+	if campaign.UserID != inputData.User.ID {
+		return campaign, errors.New("You are not the owner of this campaign")
 	}
 
 	campaign.Name = inputData.Name
