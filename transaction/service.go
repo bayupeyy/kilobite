32a6e315035d4c8c9a09db1ce76f173df441fcb5
unknown <mochamadbayuajie@gmail.com>
2024-07-19 22:09:52 +0700

    fitur transaction

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,22 @@
 package transaction
+
+type service struct {
+	repository Repository
+}
+
+type Service interface {
+	GetTransactionByCampaignID(campaignID int) ([]Transaction, error)
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetTransactionByCampaignID(campaignID int) ([]Transaction, error) {
+	transaction, err := s.repository.GetByCampaignID(campaignID)
+	if err != nil {
+		return nil, err
+	}
+	return transaction, nil
+
+}
